1863e0a45cf59702703035206d4de4da8aebb136
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-22 20:12:58 +0600

    Server to UI data send

--- index.js
+++ index.js
@@ -65,6 +65,16 @@ async function run() {
       res.send(result);
     });
 
+    // Get new added toy data
+    app.get('/addNewToy', async(req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query={email: req.query.email}
+      }
+      const result = await toyNewCollection.find().toArray();
+      res.send(result);
+    })
 
     // Add New Toy
     app.post('/addNewToy', async(req, res) => {
